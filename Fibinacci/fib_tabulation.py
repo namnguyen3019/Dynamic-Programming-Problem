08dd98e1d784d53040393809337833b0a5fbd08e
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2020-12-27 08:45:29 -0600

    grid traveler using tabulation table

--- Fibinacci/fib_tabulation.py
+++ Fibinacci/fib_tabulation.py
@@ -5,15 +5,14 @@ def fib(n):
     table[0] = 0
     table[1] = 1
 
-    for i in range(n+1):
-        if i+1 < n+1:
-            table[i+1] = table[i+1] + table[i]
+    for i in range(n):
+        table[i+1] = table[i+1] + table[i]
         if i+2 < n:
             table[i+2] = table[i+2] + table[i]
     
     return table[-1]
 
-# FIBONACCI: 0, 1, 1, 2, 3, 5, 8, 13, 21    
+# FIBONACCI: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34   
 print(fib(5))
 print(fib(8))
 print(fib(10))
---
+++ Grid-Traveler/grid_traveler_tabulation.py
@@ -0,0 +1,18 @@
+
+def gridTraveler(m, n):
+
+    table = [[0 for col in range(m+1)] for row in range(n+1)]
+
+    for j in range(1, m+1):
+        table[1][j] = 1
+    for i in range(1, n+1):
+        table[i][1] = 1
+
+    for i in range(2, n+1):
+        for j in range(2, m+1):
+            table[i][j] = table[i-1][j] + table[i][j-1]
+    return table[n][m]
+
+print(gridTraveler(3,2))
+print(gridTraveler(3,3))
+print(gridTraveler(3,4))
